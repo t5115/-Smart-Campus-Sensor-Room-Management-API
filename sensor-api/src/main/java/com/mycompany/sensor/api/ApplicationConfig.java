6cf3a0eee164ee2490cdf531e03ae92303e2b5b7
Tahmid Ahmed <tahmid.ahmed5115@gmail.com>
2026-04-07 17:54:35 +0100

    implemented a subclass of javax.ws.rs.core.Application

---
+++ sensor-api/src/main/java/com/mycompany/sensor/api/ApplicationConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sensor.api;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+/**
+ *
+ * @author tahmi
+ */
+
+@ApplicationPath("/api/v1")
+public class ApplicationConfig extends Application {
+    
+}
